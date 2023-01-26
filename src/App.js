a8574aced665a9d3eef988cca956b15765c9592c
Güven Karabulut <guven.07.568@gmail.com>
2023-01-27 00:51:38 +0300

    Add React Files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Index from './components/index';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Index />
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/containers/Container.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Container.css'
+
+function Container() {
+    return (
+        <>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-12'>
+                        <h1>Container</h1>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Container
